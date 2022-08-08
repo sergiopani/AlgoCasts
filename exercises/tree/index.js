cd7203925efbacb8c8619671c126ba4ca9cdb9d8
sergiopani <xpanisergio@gmail.com>
2022-08-08 18:02:34 +0200

    More on Trees

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,68 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data;
+        //Array to store the child nodes
+        this.children = [];
+    }
 
-class Tree {}
+    add(data){        
+        //Add a node to the children array
+        this.children.push(new Node(data));
+    }
+
+    remove(data){
+        //Througt every child of the current node        
+        this.children = this.children.filter(node => {
+            return node.data !== data
+        });
+
+    }
+
+
+}
+
+class Tree {
+    //When we first create a root alwaysis a null
+    constructor(){
+        this.root = null;
+    }
+
+    traverseBF(fn){
+        //get the root node of our tree
+        const arr = [this.root];
+
+        while(arr.length){
+            //Take out the first element of the array
+            const node = arr.shift();
+            //Add all children of the root array
+            //... => Take every element of the array and push them one by one
+            // into array
+            arr.push(...node.children);
+            
+            //Throwing waway
+            fn(node);
+        }
+
+    }
+
+
+    traverseDF(fn){
+        const arr = [this.root];
+
+        while(arr.length){
+            const node = arr.shift();
+            //Add element at the from of the array
+            arr.unshift(...node.children);
+
+            //Throwing away de node
+            fn(node);
+        }
+
+    }
+
+}
 
 module.exports = { Tree, Node };
